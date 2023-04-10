1ce4ecde8f23429c5d55dc85e25489f779ec9e77
alis-SWE <ali.cs.sheikh@gmail.com>
2023-04-10 16:54:22 -0400

    pasted routes into kombat page

--- client/src/pages/Kombat/Kombat.js
+++ client/src/pages/Kombat/Kombat.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import './Kombat.scss';
+
 const Kombat = () => {
     return ( 
         <div className="kombat">
             <h2 className="kombat__header">Kombat</h2>
+            <Link to={'/kombat/create'}>Create Kombat</Link>
+            <Link to={'/kombat/join'}>Join Kombat</Link>
         </div>
      );
 }
