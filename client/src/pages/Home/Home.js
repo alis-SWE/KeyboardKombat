5242bfcb748923722099fb45e367ee37a410ebfd
alis-SWE <ali.cs.sheikh@gmail.com>
2023-04-10 16:53:36 -0400

    added some styling

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import './Home.scss';
 import { Link } from "react-router-dom";
 
+
 const Home = () => {
     return ( 
-        <div className="home-page">
-            <h2 className="home-page__header">KeyboardKombat</h2>
+        <div className="home">
+            <h2 className="home__header">KeyboardKombat</h2>
             <Link to={'/practice'}>Practice</Link>
             <Link to={'/kombat'}>Kombat</Link>
         </div>
