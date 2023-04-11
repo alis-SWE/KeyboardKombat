1d3e3025ef629ec2f56dd4d2c99ea3bd80e771a0
alis-SWE <ali.cs.sheikh@gmail.com>
2023-04-11 05:13:44 -0400

    added progress bar styling

--- client/src/components/ProgressBar/Progressbar.jsx
+++ client/src/components/ProgressBar/Progressbar.jsx
@@ -7,25 +7,33 @@ const calcPercentage = (player, textLength) => {
     return 0;
 }
 
+
+
 const ProgressBar = ({ players, player, textLength }) => {
     const percentage = calcPercentage(player, textLength);
 
     return ( 
         <div className="progressbar">
-            <h5 className="progressbar__player">{player.name}</h5>
-            <div className="progressbar__bar" key={player._id}>
-                <div className="progressbar__bar__progress" style={{width: percentage + '%'}}>{percentage + '%'}</div>
+            <div className="progressbar__player">
+                <p className="progressbar__player__player-name">{player.name}</p>
+                <div className="progressbar__player__bar" key={player._id}>
+                    <div className="progressbar__player__bar__progress" style={{width: percentage + '%'}}>{percentage + '%'}</div>
+                </div>
+                <p className="progressbar__player__wpm">{player.wordsPerMinute + " WPM"}</p>
             </div>
+            
+
             {
                 players.map(playerObj => {
                     const percentage = calcPercentage(playerObj, textLength);
                     return playerObj._id !== player._id ? 
-                    <>
-                        <h5 className="progressbar__player">{playerObj.name}</h5>
-                        <div className="progressbar__bar" key={playerObj._id}>
-                            <div className="progressbar__bar__progress" style={{width: percentage + '%'}}>{percentage + '%'}</div>
+                    <div className="progressbar__player">
+                        <p className="progressbar__player__player-name">{playerObj.name}</p>
+                        <div className="progressbar__player__bar" key={playerObj._id}>
+                            <div className="progressbar__player__bar__progress" style={{width: percentage + '%'}}>{percentage + '%'}</div>
                         </div>
-                    </> : null
+                        <p className="progressbar__player__wpm">{playerObj.wordsPerMinute + " WPM"}</p>
+                    </div> : null
                 })
             }
         </div>
