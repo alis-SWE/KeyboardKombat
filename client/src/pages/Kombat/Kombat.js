b04d0dbb5ef63f5cf9a683c00fcf278ce6b47f00
alis-SWE <ali.cs.sheikh@gmail.com>
2023-04-11 05:21:18 -0400

    added styling to kombat page

--- client/src/pages/Kombat/Kombat.js
+++ client/src/pages/Kombat/Kombat.js
@@ -4,9 +4,8 @@ import './Kombat.scss';
 const Kombat = () => {
     return ( 
         <div className="kombat">
-            <h2 className="kombat__header">Kombat</h2>
-            <Link to={'/kombat/create'}>Create Kombat</Link>
-            <Link to={'/kombat/join'}>Join Kombat</Link>
+            <Link className="kombat__create" to={'/kombat/create'}>Create Kombat</Link>
+            <Link className="kombat__join" to={'/kombat/join'}>Join Kombat</Link>
         </div>
      );
 }
